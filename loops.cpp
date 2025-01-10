5ae3cb3ae2e0cb45dc77a5e7bded562dcac4b261
Asuna XD <ok4625685@gmail.com>
2025-01-10 19:20:13 +0530

    questions

--- loops.cpp
+++ loops.cpp
@@ -1,123 +1,139 @@
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     1. Sum of N Natural Numbers:
-// Write a C++ program that:
-// Takes an integer n as input.
-// Uses a loop to calculate the sum of all natural numbers from 1 to n.
-// Outputs the sum.
+#include <iostream>
+using namespace std;
+int main()
+{
+    //     1. Sum of N Natural Numbers:
+    // Write a C++ program that:
+    // Takes an integer n as input.
+    // Uses a loop to calculate the sum of all natural numbers from 1 to n.
+    // Outputs the sum.
 
-// int n;
-// cout << "Enter the number : " << endl;
-// cin >> n;
-// int sum = 0;
-// for (int i = 1; i <= n; i++)
-// {
-//     sum += i;
-// }
-// cout << "The sum of given number is :" << sum << endl;
+    // int n;
+    // cout << "Enter the number : " << endl;
+    // cin >> n;
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+    // cout << "The sum of given number is :" << sum << endl;
 
-//  2. Factorial of a Number:
-// Write a C++ program that:
-// Takes an integer n as input.
-// Uses a loop to calculate the factorial of n.
-// Outputs the result.
+    //  2. Factorial of a Number:
+    // Write a C++ program that:
+    // Takes an integer n as input.
+    // Uses a loop to calculate the factorial of n.
+    // Outputs the result.
 
-// int n;
-// cout << "Enter the number " << endl;
-// cin >> n;
-// int sum = 1;
+    // int n;
+    // cout << "Enter the number " << endl;
+    // cin >> n;
+    // int sum = 1;
 
-// for (int i = 1; i <= n; i++)
-// {
-//     sum *= i;
-// }
-// cout << "the factorial of:" << sum << endl;
-// multiplication table`
-// int n;
-// cout << "enter the number : " << endl;
-// cin >> n;
-// for (int i = 1; i <= 10; i++)
-// {
-//     cout << n * i << endl;
-// }
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum *= i;
+    // }
+    // cout << "the factorial of:" << sum << endl;
+    // multiplication table`
+    // int n;
+    // cout << "enter the number : " << endl;
+    // cin >> n;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << n * i << endl;
+    // }
 
-//     1. Print Numbers from 1 to N:
-// Write a C++ program that:
+    //     1. Print Numbers from 1 to N:
+    // Write a C++ program that:
 
-// Takes a number n as input.
-// Uses a loop to print all numbers from 1 to n.
+    // Takes a number n as input.
+    // Uses a loop to print all numbers from 1 to n.
 
-// int n;
-// cout << "enter the number: " << endl;
-// cin >> n;
-// for (int i = 1; i <= n; i++)
-// {
-//     cout << i << endl;
-// }
+    // int n;
+    // cout << "enter the number: " << endl;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout << i << endl;
+    // }
 
-//    3  Even Numbers from 1 to N:
-// Write a C++ program that:
+    //    3  Even Numbers from 1 to N:
+    // Write a C++ program that:
 
-// Takes a number n as input.
-// Uses a loop to print all even numbers from 1 to n.
+    // Takes a number n as input.
+    // Uses a loop to print all even numbers from 1 to n.
 
-// int n;
-// cout << "enter the number" << endl;
-// cin >> n;
+    // int n;
+    // cout << "enter the number" << endl;
+    // cin >> n;
 
-// for (int i = 2; i <= n; i += 2)
-// {
-//     cout << i << endl;
-// }
+    // for (int i = 2; i <= n; i += 2)
+    // {
+    //     cout << i << endl;
+    // }
 
-// 4. Odd Numbers from 1 to N:
-// Write a C++ program that:
+    // 4. Odd Numbers from 1 to N:
+    // Write a C++ program that:
 
-// Takes a number n as input.
-// Uses a loop to print all odd numbers from 1 to n.
+    // Takes a number n as input.
+    // Uses a loop to print all odd numbers from 1 to n.
 
-// int a;
-// cout << "enter the number: " << endl;
-// cin >> a;
-// for (int i = 1; i <= a; i += 2)
-// {
-//     cout << i << endl;
-//     ;
-// }
+    // int a;
+    // cout << "enter the number: " << endl;
+    // cin >> a;
+    // for (int i = 1; i <= a; i += 2)
+    // {
+    //     cout << i << endl;
+    //     ;
+    // }
 
-// 6. Reverse a Number:
-// Write a C++ program that:
-// Takes a positive integer n as input.
-// Uses a loop to reverse the digits of the number and prints the reversed number.
+    // 6. Reverse a Number:
+    // Write a C++ program that:
+    // Takes a positive integer n as input.
+    // Uses a loop to reverse the digits of the number and prints the reversed number.
 
-// int n, reverse = 0;
-// cout << "please enter the number : " << endl;
-// cin >> n;
+    // int n, reverse = 0;
+    // cout << "please enter the number : " << endl;
+    // cin >> n;
 
-// while (n != 0)
-// {
-//     int a = n % 10;
-//     reverse = reverse * 10 + a;
-//     n / 10;
-// }
-// cout << reverse << endl;
-//
+    // while (n != 0)
+    // {
+    //     int a = n % 10;
+    //     reverse = reverse * 10 + a;
+    //     n / 10;
+    // }
+    // cout << reverse << endl;
+    //
 
-// 8. Count Digits in a Number :
-// Write a C++ program that:
-// Takes a number n as input.
-// Uses a loop to count the number of digits in the number and outputs the count.
-//     int n,
-//         count = 0;
-//     cout << "enter the number: ";
-//     cin >> n;
-//     while (n != 0)
-//     {
-//         n /= 10;
-//         count++;
-//     }
-//     cout << count << endl;
-//     return 0;
-// }
+    // 8. Count Digits in a Number :
+    // Write a C++ program that:
+    // Takes a number n as input.
+    // Uses a loop to count the number of digits in the number and outputs the count.
+    //     int n,
+    //         count = 0;
+    //     cout << "enter the number: ";
+    //     cin >> n;
+    //     while (n != 0)
+    //     {
+    //         n /= 10;
+    //         count++;
+    //     }
+    //     cout << count << endl;
+
+    //     12. Sum of Digits of a Number:
+    // Write a C++ program that:
+
+    // Takes a number n as input.
+    // Uses a loop to calculate the sum of the digits of the number and outputs the sum.
+
+    int a;
+    cout << "enter the number " << endl;
+    cin >> a;
+
+    while (n !)
+    {
+        /* code */
+    }
+
+    return 0;
+}
